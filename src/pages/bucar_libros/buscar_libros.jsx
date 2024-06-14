fddaccce92b0eeda4778893ae3281f49e2ea0ce3
annymane <stpmansill@upana.edu.gt>
2024-06-13 19:48:16 -0600

    Problema solucionado

--- src/pages/bucar_libros/buscar_libros.jsx
+++ src/pages/bucar_libros/buscar_libros.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import styles from './buscar_libros.module.css'; // Importa estilos usando CSS Modules
 
 export default function BuscarLibros() {
@@ -7,6 +8,8 @@ export default function BuscarLibros() {
     const [currentPage, setCurrentPage] = useState(1);
     const [librosPerPage] = useState(50);
     const [errorMensaje, setErrorMensaje] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
         cargarLibros();
@@ -22,10 +25,16 @@ export default function BuscarLibros() {
             });
     };
 
+    // Filtrado de libros
+    const librosFiltrados = libros.filter(libro => 
+        libro.titulo.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        libro.autor.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     // Paginación
     const indexOfLastLibro = currentPage * librosPerPage;
     const indexOfFirstLibro = indexOfLastLibro - librosPerPage;
-    const librosActuales = libros.slice(indexOfFirstLibro, indexOfLastLibro);
+    const librosActuales = librosFiltrados.slice(indexOfFirstLibro, indexOfLastLibro);
 
     const paginate = pageNumber => setCurrentPage(pageNumber);
 
@@ -40,7 +49,7 @@ export default function BuscarLibros() {
             setErrorMensaje('Libro agregado a la biblioteca personal');
             setTimeout(() => {
                 setErrorMensaje('');
-            }, 2000); // Desaparece el mensaje después de 2 segundos
+            }, 2500); // Desaparece el mensaje después de 2 segundos
         })
         .catch(error => {
             if (error.response && error.response.status === 409) {
@@ -62,6 +71,18 @@ export default function BuscarLibros() {
     return (
         <div className={styles.container}>
             <h2>Lista de Libros</h2>
+            <div className={styles.searchContainer}>
+                <input 
+                    type="text"
+                    placeholder="Buscar por título o autor"
+                    value={searchTerm}
+                    onChange={e => setSearchTerm(e.target.value)}
+                    className={styles.searchInput}
+                />
+                <button onClick={() => navigate('/home')} className={styles.button}>
+                    Regresar
+                </button>
+            </div>
             {errorMensaje && <div className={`${styles.alert} ${styles['alert-danger']}`}>{errorMensaje}</div>}
             <div className={styles['table-responsive']}>
                 <table className={`${styles.table} ${styles['table-hover']} ${styles['table-striped']}`}>
@@ -98,7 +119,7 @@ export default function BuscarLibros() {
                 </table>
             </div>
             <ul className={styles.pagination}>
-                {Array.from({ length: Math.ceil(libros.length / librosPerPage) }).map((_, index) => (
+                {Array.from({ length: Math.ceil(librosFiltrados.length / librosPerPage) }).map((_, index) => (
                     <li key={index + 1} className={`${styles['pagination-item']} ${index + 1 === currentPage ? styles.active : ''}`} onClick={() => paginate(index + 1)}>
                         {index + 1}
                     </li>
